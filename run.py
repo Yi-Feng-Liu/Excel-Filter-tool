9bf7946521bb01b1961e56721b20f32b45244ca3
Yi-Feng <a124883538@gmail.com>
2023-04-13 23:13:05 +0800

    Update

--- run.py
+++ run.py
@@ -6,9 +6,24 @@ from tkinter.font import Font
 import openpyxl
 import pandas as pd
 from tkinter import ttk
+import numpy as np
+
 
 global filepath
 filepath = ""
+
+
+def resize(imgpath:str, resize_w:int, resize_h:int, changeBG=True):
+    image_name = imgpath.split("\\")[-1].split(".")[0]
+    origin_img = cv.imread(imgpath)
+    # replace the iamge white background to #d2efd3
+    if changeBG:
+        origin_img[np.where((origin_img == [255, 255, 255]).all(axis=2))] = [211, 239, 211]
+    resize_img = cv.resize(origin_img, (resize_w, resize_h))
+    save_path = f'D:\\GUI\\Test\\{image_name}_resize.jpg'
+    cv.imwrite(save_path, resize_img)
+    return save_path
+
 
 def finishInfo():
     messagebox.showinfo(title="通知", message="已篩選完成")
@@ -17,7 +32,7 @@ def openFile():
     filepath = filedialog.askopenfilename(title="選擇檔案", filetypes=[("Excel files", ".xlsx .xls")])
     
     if (filepath):
-        label.config(text=f'選取的檔案路徑為\n {filepath}')
+        label.config(text=f'選取的檔案路徑為\n\n {filepath}')
         # messagebox.showinfo(title="通知", message="選擇成功")
         comfirm_button["state"] = "active"
     else:
@@ -33,7 +48,9 @@ def processExcel():
     else:
         messagebox.showerror(title="錯誤", message="請選擇檔案類型")
 
-bgcolor = "#ffffff"
+#  #ffffff #d2efd3 #cdd6cd
+
+bgcolor = "#d2efd3"
 
 root = Tk()
 font = Font(family="微軟正黑體", size=12)
@@ -45,19 +62,23 @@ root.attributes('-alpha', 1)
 root.title("Excel Filter Tool")
 
 # set icon
-img = Image.open('123.jpg')
-icon = ImageTk.PhotoImage(img) 
+iconimg = Image.open('D:\\GUI\\Test\\icon.jpg')
+icon = ImageTk.PhotoImage(iconimg) 
 root.iconphoto(True, icon)
 root.configure(bg=bgcolor)
- 
+
 greet = Label(root, text="選擇檔案類型", height="3", bg=bgcolor, font=font)
 greet.pack()
 
-# set the img at right down
-img2 = Image.open('456.jpg')
+# read the img and set the img at right down
+origin_img_path = resize('D:\\GUI\\Test\\456.jpg', resize_w=100, resize_h=100, changeBG=True)
+img2 = Image.open(origin_img_path)
 icon2 = ImageTk.PhotoImage(img2) 
+width = icon2.width()
+height = icon2.height()
+
 pikachu = Label(root, image=icon2)
-pikachu.pack(side='bottom', padx=1, pady=2)
+pikachu.place(x=500-width, y=400-height)
 
 # options list
 options = ['代謝症候群','選項2','選項3','選項4','選項5', '選項6', '選項7', '選項8', '選項9', '選項10', '選項11']
@@ -73,7 +94,11 @@ select_button.pack(pady=10)
 label = Label(root, text='', font=font, bg=bgcolor)
 label.pack(pady=10)
 
-comfirm_button = Button(root, text="確認", command=processExcel, bg='#f09595', )
+
+button_img = resize('D:\\GUI\\Test\\confirm.jpg', resize_w=70, resize_h=60, changeBG=False)
+button_img = Image.open(button_img)
+comfirm_button_img = ImageTk.PhotoImage(button_img) 
+comfirm_button = Button(root, text="確認", command=processExcel, image=comfirm_button_img)
 comfirm_button.pack(pady=10)
 comfirm_button["state"] = "disabled"
 
