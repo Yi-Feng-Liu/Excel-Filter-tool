5436d51110965461015eb8438705cf81e592654a
Yi Feng Liu <73655055+Yi-Feng-Liu@users.noreply.github.com>
2023-04-20 17:30:40 +0800

    Update process.py

--- process.py
+++ process.py
@@ -7,7 +7,10 @@ import time
 
 
 class Judge_Metabolic_Syndrome:
-    def __init__(self):
+    def __init__(self, io, select_years, dst_worksheet):
+        self.io = io
+        self.dst_worksheet = dst_worksheet
+        self.select_years = select_years
         self.standard_waistline = 90
         self.standard_systolic_blood_pressure = 130
         self.standard_diastolic_blood_pressure = 85
@@ -18,6 +21,7 @@ class Judge_Metabolic_Syndrome:
         self.metabolic_syndrome_column = [5, 9, 10, 11, 12, 14, 15]
         self.fill = PatternFill(start_color='FFC7CE', end_color='FFC7CE', fill_type='solid')
         self.font = Font(color='FF0000')
+        self.main_procesdure()
 
     def change_date_time(self, worksheet, number_of_column):
         """Remove hours:minute:second format of the datetime 
@@ -57,10 +61,6 @@ class Judge_Metabolic_Syndrome:
         return worksheet
 
 
-    def set_red_font(self, val):
-        return 'color: red'
-
-
     def change_font_color_format(self, cell):
         """Change cell color and font color 
 
@@ -68,6 +68,7 @@ class Judge_Metabolic_Syndrome:
             cell : the cell coordinate
         """
         cell.font = self.font
+
 
     def label_over_standard(self, worksheet):
         """Use to label the cell, if cell's value exceed the standard
@@ -135,21 +136,7 @@ class Judge_Metabolic_Syndrome:
         return worksheet
     
 
-    def copy_format_from_sheet1(self, io, dst_worksheet:str):
-        """Copy the original sheet header format to specific sheet
-
-        Including cell's fill, font, color, alignment, dimensions.
-
-        Args:
-            io (str): Fila path
-            dst_worksheet (str): Name of saving sheet
-        """
-        workbook = openpyxl.load_workbook(io)
-        
-        ws1 = workbook['健檢資料']
-        ws2 = workbook[dst_worksheet]
-        # copy format sheet1 header to sheet2 header
-        
+    def copy_title_format(self, ws1, ws2):
         for row in ws1.iter_rows(min_row=1, max_row=1):
             for cell in row:
                 # copy cell
@@ -160,9 +147,20 @@ class Judge_Metabolic_Syndrome:
                 ws2[cell.coordinate].number_format = copy.copy(cell.number_format)
                 ws2.row_dimensions[cell.row].height = copy.copy(ws1.row_dimensions[cell.row].height)
                 ws2.column_dimensions[cell.column_letter].width = copy.copy(ws1.column_dimensions[cell.column_letter].width)
+        return ws2
+    
+    def copy_format_from_sheet1(self):
+        """Copy the original sheet header format to specific sheet
+
+        Including cell's fill, font, color, alignment, dimensions.
+        """
+        workbook = openpyxl.load_workbook(self.io)
         
+        ws1 = workbook['健檢資料']
+        ws2 = workbook[self.dst_worksheet]
+        # copy format sheet1 header to sheet2 header
+        ws2 = self.copy_title_format(ws1=ws1, ws2=ws2)
         ws2 = self.set_specific_column_format(worksheet=ws2, eng_column='U')
-        time.sleep(0.5)
         ws2 = self.change_date_time(worksheet=ws2, number_of_column=7)
         ws2 = self.place_center(worksheet=ws2)
 
@@ -170,24 +168,25 @@ class Judge_Metabolic_Syndrome:
         # ws1 = self.label_over_standard(worksheet=ws1)
         # only process new sheet
         ws2 = self.label_over_standard(worksheet=ws2)
-        workbook.save(io)
+        workbook.save(self.io)
         print("saved")
 
 
-    def process_Metabolic_Syndrome(self, io:str, select_years:str, dst_worksheet:str):
-        """篩選代謝症候群的Excel檔案
+    def read_file(self):
+        df = pd.read_excel(self.io, sheet_name='健檢資料', engine='openpyxl')
+        return df
+    
+    
+    def process_Metabolic_Syndrome(self, df:pd.DataFrame):
+        """Filter file and add new column named 超過標準數,
 
         Args:
-            io (str): Fila path
-            src_worksheet (str): Name of process sheet
-            dst_worksheet (str): Name of saving sheet
+            df (pd.DataFrame): Original dataframe
+        Returns:
+            dataframe
         """
-
-        df = pd.read_excel(io, sheet_name='健檢資料', engine='openpyxl')
-        
         df['超過標準數'] = 0
-        
-        df = df[df['年度代碼'].str.startswith(select_years)]
+        df = df[df['年度代碼'].str.startswith(self.select_years)]
              
         for i in range(len(df.index)):
             gender = df.iloc[i,self.metabolic_syndrome_column[0]] 
@@ -227,25 +226,84 @@ class Judge_Metabolic_Syndrome:
                 if hdlc < self.hdlc+10:
                     over_standard_cnt += 1              
                 df.iloc[i, len(df.columns)-1] = over_standard_cnt
-
-        # 將不符合的drop
-        # for i in range(len(df['超過標準數'])):
-        #     if df['超過標準數'][i] < 3:
-        #         df = df.drop(labels=i, axis=0)
-        
         df = df.sort_values(by=['超過標準數'], ascending=False)
-        # 建立一個新的 ExcelWriter 物件
-        writer = pd.ExcelWriter(io, mode='a', engine='openpyxl', if_sheet_exists='replace')
-        df.to_excel(writer, sheet_name=dst_worksheet, index=False)
-        writer.close() 
         
-        self.copy_format_from_sheet1(io=io, dst_worksheet=dst_worksheet)
+        return df
+        
+    
+    def save_file_and_copy_title(self, df):
+        # 建立一個新的 ExcelWriter 物件
+        writer = pd.ExcelWriter(self.io, mode='a', engine='openpyxl', if_sheet_exists='replace')
+        df.to_excel(writer, sheet_name=self.dst_worksheet, index=False)
+        writer.close() 
+        self.copy_format_from_sheet1()
 
 
+    def main_procesdure(self):
+        df = self.read_file()
+        df = self.process_Metabolic_Syndrome(df)
+        self.save_file_and_copy_title(df)
+
+
+
+class Metabolic_Syndrome_From_Summary(Judge_Metabolic_Syndrome):
+    def __init__(self, summary_file_fath, select_years, saving_file_path, save_sheet_name):
+        super().__init__()
+        self.summary_file_fath = summary_file_fath
+        self.saving_file_path = saving_file_path
+        self.select_years = select_years
+        self.save_sheet_name = save_sheet_name
+        self.df = pd.read_excel(summary_file_fath, sheet_name='工作表1', engine='openpyxl')
+        self.df2 = pd.read_excel(saving_file_path, sheet_name='健檢資料', engine='openpyxl')
+        self.goal_column_name = self.df2.columns.tolist()
+        self.main_procesdure()
+        
+
+    def append_column(self):
+        if 'SGPT' not in self.df2.columns:
+            self.df2['SGPT'] = 0
+        if 'SGOT' not in self.df2.columns:
+            self.df2['SGOT'] = 0
+        self.goal_column_name = self.df2.columns.tolist()
+        
+        return self.goal_column_name
+    
+    def change_column_name(self):
+        self.goal_column_name = self.append_column()
+        self.df['年度代碼'] = '111年度體檢'
+        self.df['部門代號'] = 'X001'
+        self.df['健檢過程備註說明'] = ''
+        self.df = self.df.drop(labels=0, axis=0)
+        
+        specific_column = ['年度代碼', '姓名', '工/學號', '部門代號', '部門/科系', '性別', '年齡', '身高', '體重', '腰圍', '收縮壓', '舒張壓', 'AC飯前血糖', 'T-CHO總膽固醇', 'TG三酸甘油脂', 'HDL高密度脂蛋白', 'LDL低密度脂蛋白', '請問您過去一個月內是否有吸菸？', '既往病史', '健檢過程備註說明', 'SGOT血清麩酸草酸轉氨脢', 'SGPT血清麩酸丙銅轉氨脢']
+
+        speific_df = self.df[specific_column].copy()
+        speific_df_columns = speific_df.columns.to_list()
+
+        for i in range(len(speific_df_columns)):
+            speific_df.rename(columns={speific_df_columns[i]: self.goal_column_name[i]}, inplace=True)  
+
+        # save the sheet 
+        # self.process_Metabolic_Syndrome(io='test.xlsx', select_years=111, dst_worksheet='test1')
+
+        return speific_df
+    
+    def main_procesdure(self):
+        # writer = pd.ExcelWriter(self.saving_file_path , mode='a', engine='openpyxl', if_sheet_exists='replace')
+        # speific_df.to_excel(writer, sheet_name=self.save_sheet_name, index=False)
+        # writer.close()
+        speific_df = self.change_column_name()
+        speific_df = self.process_Metabolic_Syndrome(speific_df)
+        self.save_file_and_copy_title(speific_df)
+        print('OK')
+        
+        
+        
+    
 def main():
-    Judge_Metabolic_Syndrome().process_Metabolic_Syndrome('111.xlsx', select_years='111', dst_worksheet='工作表1')
-    print("Finish")
+    # Judge_Metabolic_Syndrome('test.xlsx', '111', '工作表1')
+    Metabolic_Syndrome_From_Summary(summary_file_fath='一般作業總表.xlsx', select_years='111', saving_file_path='test.xlsx', save_sheet_name='test_data')
+    # print("Finish")
 
 if __name__ == '__main__':
-
     main()
