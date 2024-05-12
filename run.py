a1dc6c990ed4f672a5a1912fe0122ea61186eff1
Yi-Feng <a124883538@gmail.com>
2024-05-12 18:49:11 +0800

    update

--- run.py
+++ run.py
@@ -4,13 +4,7 @@ from util.GUI import Excel_GUI
 
 def main():
     root = tk.Tk()
-    Excel_GUI(
-        root
-        # personal_token='personal_token', 
-        # repo_name='Yi-Feng-Liu/Excel-Filter-Tool', 
-        # release_name='v2.0.0', 
-        # download_rar_name='Widget.rar'
-    )
+    Excel_GUI(root)
     root.mainloop()
 
 
